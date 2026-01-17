4aa9792e7cc45324467581b06501976a3842f21f
buildwithjaym <jaymmaruji@gmail.com>
2026-01-17 16:24:13 +0800

    Just try the homepage

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opsdesk Practice</title>
+</head>
+<style>
+
+    *{
+    background: blue;
+    font-family: Arial;
+    }
+    .Home{
+        color: Violet;
+        align-items: center;
+        justify-content: center;
+    }
+    .Home::after{
+        color: white;
+    }
+
+</style>
+<body>
+<p id = "Home">This is our Homepage </p>
+</body>
+</html>
