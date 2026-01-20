a443018471999c2f0cb8f41512a7b7ff2473c056
buildwithjaym <jaymmaruji@gmail.com>
2026-01-20 20:40:09 +0800

    Add my database connection

---
+++ src/db.php
@@ -0,0 +1,15 @@
+<?php
+$DB_HOST = "127.0.0.1";
+$DB_USER = "root";
+$DB_PASS = "";
+$DB_NAME = "fam_db"; //This is my database name
+
+$conn = mysqli_connect ($DB_HOST,$DB_USER, $DB_PASS, $DB_NAME);
+
+if (!$conn){
+    die("Database connection failed: " . mysqli_connect_error());
+}
+else{
+    echo("Database connected");
+}
+?>
