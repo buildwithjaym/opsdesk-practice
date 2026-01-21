2c230a674123fcb01b026f96491a01942614ac23
buildwithjaym <jaymmaruji@gmail.com>
2026-01-21 13:30:32 +0800

    Edited the homepage with minimal edits

--- public/index.php
+++ public/index.php
@@ -9,12 +9,46 @@ if (!isset($_SESSION["user_id"])) {
 <!DOCTYPE html>
 <html>
 <head>
+<style>
+:root{
+  --blue-6: #b45dee34;
+  --blue-1:#0ea5e9;
+  --blue-2:#2563eb;
+
+}
+body{
+  margin:0;
+  min-height: 100vh;
+  background: linear-gradient(135deg, var(--blue-1), var(--blue-2));
+  padding: 10px;
+  font-family: Arial, sans-serif;
+  overflow:hidden;
+  color: #ffff;
+  display:flex;
+
+}
+a {
+  text-decoration: none;
+  color: #ffff;
+  font-weight: 10px;
+}
+#header{
+  min-height:90vh;
+  align-items: space-in-between;
+
+}
+
+
+
+</style> 
   <meta charset="UTF-8">
-  <title>OpsDesk</title>
+  <title>Ticketing System</title>
 </head>
 <body>
+  <div id="header">
   <h2>Welcome, <?php echo htmlspecialchars($_SESSION["fullname"]); ?>!</h2>
   <p>Role: <?php echo htmlspecialchars($_SESSION["role"]); ?></p>
   <p><a href="logout.php">Logout</a></p>
+</div>
 </body>
 </html>
