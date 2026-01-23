72c6a4dd37c1f4e482d52a95b1b62b380e21b052
buildwithjaym <jaymmaruji@gmail.com>
2026-01-23 22:41:44 +0800

    Edited db

--- src/db.php
+++ src/db.php
@@ -2,7 +2,7 @@
 $DB_HOST = "127.0.0.1";
 $DB_USER = "root";
 $DB_PASS = "";
-$DB_NAME = "fam_db"; //This is my database name
+$DB_NAME = "fam_db"; 
 
 $conn = mysqli_connect ($DB_HOST,$DB_USER, $DB_PASS, $DB_NAME);
 
